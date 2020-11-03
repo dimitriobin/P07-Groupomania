d894336719e599c93e4d246ec915850bfa4f35e5
Dimitri Obin <dimitriobin@gmail.com>
2020-11-03 15:55:17 +0100

    connection à la base de donnée

--- back_end/app.js
+++ back_end/app.js
@@ -1,13 +1,24 @@
-const express = require('express')
+const express = require('express');
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const usersRoute = require('./routes/users')
-const postsRoute = require('./routes/posts')
+const usersRoute = require('./routes/users');
+const postsRoute = require('./routes/posts');
 
 const app = express();
 
+// Connect to database
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+db.authenticate()
+.then(() => console.log('Database connected ...'))
+.catch(err => console.log(err));
+
 // Body parsers
-app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.json());
+app.use(express.urlencoded());
 
 
 // Routes
