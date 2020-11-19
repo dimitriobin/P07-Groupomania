d66a40c78ca94da991cd9f15a89433547911283f
Dimitri Obin <dimitriobin@gmail.com>
2020-11-19 14:35:32 +0100

    Création de la view PersonalAccount.vue

---
+++ front_end/src/views/PersonalAccount.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="PersonalAccount">
+    <h1>Here is the Personal Account page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'PersonalAccount',
+  components: {},
+};
+</script>
+
+<style scoped>
+  body {
+    background: black;
+  }
+</style>
