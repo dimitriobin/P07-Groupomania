9bd4baf5123e0b3b1df37d2efc3ccc875d223716
Dimitri Obin <dimitriobin@gmail.com>
2020-11-19 14:40:43 +0100

    Ajout de borders CSS pour y voir plus clair sur le layout

--- front_end/src/App.vue
+++ front_end/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <Header />
-    <router-view />
+    <div class="container">
+      <router-view />
+    </div>
   </div>
 </template>
 
@@ -23,18 +25,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  .container {
+    border: 1px solid black;
   }
 }
 </style>
