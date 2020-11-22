908655ca650031e357f0c2bd7c2049469500eba7
Dimitri Obin <dimitriobin@gmail.com>
2020-11-22 15:21:23 +0100

    integration de la rubrique top sujets dans la sidebar

--- front_end/src/components/Sidebar.vue
+++ front_end/src/components/Sidebar.vue
@@ -1,8 +1,31 @@
 <template>
-  <div id="Sidebar" class="border border-dark">
-    <p>Here will be printed some suggestions of subjects or users to follow</p>
-    <router-link to="/subjects">Subjects</router-link> |
-  </div>
+  <b-row no-gutters tag="aside" id="Sidebar" class="border border-dark">
+    <b-col cols="12" class=" p-4 rounded-lg bg-white">
+      <h2 class="h5 text-muted text-left font-weight-bold mb-3">Top des sujets</h2>
+      <b-row no-gutters tag="ol" class="px-5">
+        <b-col cols="12" class="d-flex">
+          <li class="text-left flex-grow-1 font-weight-bold">News</li>
+          <b-link href="#" class="text-right">Suivre</b-link>
+        </b-col>
+        <b-col cols="12" class="d-flex">
+          <li class="text-left flex-grow-1 font-weight-bold">News</li>
+          <b-link href="#" class="text-right">Suivre</b-link>
+        </b-col>
+        <b-col cols="12" class="d-flex">
+          <li class="text-left flex-grow-1 font-weight-bold">News</li>
+          <b-link href="#" class="text-right">Suivre</b-link>
+        </b-col>
+        <b-col cols="12" class="d-flex">
+          <li class="text-left flex-grow-1 font-weight-bold">News</li>
+          <b-link href="#" class="text-right">Suivre</b-link>
+        </b-col>
+      </b-row>
+      <router-link to="/subjects"
+        class="btn btn-danger text-white font-weight-bold mx-auto">
+        Tous les sujets
+      </router-link>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
@@ -13,4 +36,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+#Sidebar {
+    a {
+        color: #00B3D9;
+    }
+}
 </style>
