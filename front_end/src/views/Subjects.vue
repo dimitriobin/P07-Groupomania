84798c9de0484832326183eefde72855c1ec656f
Dimitri Obin <dimitriobin@gmail.com>
2020-11-23 07:19:50 +0100

    creation des listes avec des noms factices

--- front_end/src/views/Subjects.vue
+++ front_end/src/views/Subjects.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>Here there's a list of all subjects existing</h1>
+    <h1 class="h2 my-5">Découvrez le top des sujets parmis ce classement</h1>
+    <b-list-group  tag="ul" class="text-left w-75 mx-auto my-5">
+      <b-list-group-item tag="li" class="h4 m-0 py-3 pl-5">
+        1 <b-img src="https://picsum.photos/50" rounded="circle" class="ml-3"></b-img> Cras justo odio
+      </b-list-group-item>
+      <b-list-group-item tag="li" class="h4 m-0 py-3 pl-5">
+        2 <b-img src="https://picsum.photos/50" rounded="circle" class="ml-3"></b-img> Dapibus ac facilisis in
+      </b-list-group-item>
+      <b-list-group-item tag="li" class="h4 m-0 py-3 pl-5">
+        3 <b-img src="https://picsum.photos/50" rounded="circle" class="ml-3"></b-img> Morbi leo risus
+      </b-list-group-item>
+      <b-list-group-item tag="li" class="h4 m-0 py-3 pl-5">
+        4 <b-img src="https://picsum.photos/50" rounded="circle" class="ml-3"></b-img> Porta ac consectetur ac
+      </b-list-group-item>
+    </b-list-group>
   </div>
 </template>
