1dcbd1dd11ff626dd0eddab0a387111926d1bcea
Dimitri Obin <dimitriobin@gmail.com>
2020-11-23 11:10:30 +0100

    responsive mobile tablette et desktop

--- front_end/src/views/Subject.vue
+++ front_end/src/views/Subject.vue
@@ -1,8 +1,22 @@
 <template>
-  <b-row id="Subject" >
-    <b-col cols="12">
-      <b-img fluid-grow src="https://picsum.photos/800/200"></b-img>
-      <h1 class="mb-4 bg-white rounded-lg p-2 font-weight-bold">Here is the subject title</h1>
+  <b-row  id="Subject" >
+    <b-col tag="header" cols="12">
+      <div class=" bg-white shadow rounded-lg mb-4">
+        <b-img fluid-grow src="https://picsum.photos/800/200"></b-img>
+        <h1 class="font-weight-bold py-3">Here is the subject title</h1>
+      </div>
+    </b-col>
+    <b-col cols="12" lg="4" tag="aside" order-lg="3">
+      <div class="bg-white shadow rounded-lg p-4 text-left mb-4">
+        <p><small>Crée le 16 janvier 2021</small></p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Aperiam totam illo nulla, iure quas sed explicabo velit,
+            mollitia id iste placeat dolorum libero dicta natus minus
+            autem enim qui harum.</p>
+        <p class="d-inline-block mr-5 mr-lg-5"><span class="h4">320</span><br>Membres</p>
+        <p class="d-inline-block ml-5 ml-lg-5"><span class="h4">15</span><br>en ligne</p>
+        <b-button block variant="info">Rejoindre</b-button>
+      </div>
     </b-col>
     <b-col cols="12" lg="8">
       <SortingNav />
@@ -11,17 +25,6 @@
       <Post />
       <Post />
       <Post />
-    </b-col>
-    <b-col cols="12" lg="4" tag="aside" class="bg-white shadow rounded-lg p-4 h-100 text-left">
-      <h2 class="h3 mb-3">A propos du sujet</h2>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-         Aperiam totam illo nulla, iure quas sed explicabo velit,
-          mollitia id iste placeat dolorum libero dicta natus minus
-           autem enim qui harum.</p>
-      <p class="d-inline-block mr-5"><span class="h4">320</span><br>Membres</p>
-      <p class="d-inline-block ml-5"><span class="h4">15</span><br>en ligne</p>
-      <b-button block variant="info">Rejoindre</b-button>
-      <p><small>Crée le 16 janvier 2021</small></p>
     </b-col>
   </b-row>
 </template>
