7ded5e67d60029bd97b7150d322f9848fef9d9e2
Dimitri Obin <dimitriobin@gmail.com>
2020-11-23 10:26:14 +0100

    intégration du composant Post et creation de la Sidebar

--- front_end/src/components/Sidebar.vue
+++ front_end/src/components/Sidebar.vue
@@ -1,23 +1,23 @@
 <template>
   <b-row no-gutters tag="aside" id="Sidebar">
-    <b-col cols="12" class="p-4 rounded-lg bg-white mb-3">
+    <b-col cols="12" class="p-4 rounded-lg bg-white mb-3 shadow">
       <h2 class="h5 text-muted text-left font-weight-bold mb-3">Top des sujets</h2>
       <b-row no-gutters tag="ol" class="px-md-3 px-lg-5">
         <b-col cols="12" class="d-flex">
           <li class="text-left flex-grow-1 font-weight-bold">News</li>
-          <b-link to="/user" class="text-right">Suivre</b-link>
+          <b-link to="/subject" class="text-right">Suivre</b-link>
         </b-col>
         <b-col cols="12" class="d-flex">
           <li class="text-left flex-grow-1 font-weight-bold">Politics</li>
-          <b-link to="/user" class="text-right">Suivre</b-link>
+          <b-link to="/subject" class="text-right">Suivre</b-link>
         </b-col>
         <b-col cols="12" class="d-flex">
           <li class="text-left flex-grow-1 font-weight-bold">Dogs</li>
-          <b-link to="/user" class="text-right">Suivre</b-link>
+          <b-link to="/subject" class="text-right">Suivre</b-link>
         </b-col>
         <b-col cols="12" class="d-flex">
           <li class="text-left flex-grow-1 font-weight-bold">Humor</li>
-          <b-link to="/user" class="text-right">Suivre</b-link>
+          <b-link to="/subject" class="text-right">Suivre</b-link>
         </b-col>
       </b-row>
       <router-link to="/subjects"
@@ -25,7 +25,7 @@
         Tous les sujets
       </router-link>
     </b-col>
-    <b-col cols="12" class=" p-4 rounded-lg bg-white">
+    <b-col cols="12" class=" p-4 rounded-lg bg-white  shadow">
       <h2 class="h5 text-muted text-wrap text-left font-weight-bolder mb-3">
         Visitez les profils de vos collègues
       </h2>
--- front_end/src/views/Subject.vue
+++ front_end/src/views/Subject.vue
@@ -1,16 +1,42 @@
 <template>
-  <div class="Subject">
-    <h1>Here are all Posts and Comments for a specific subject</h1>
-  </div>
+  <b-row id="Subject" >
+    <b-col cols="12">
+      <b-img fluid-grow src="https://picsum.photos/800/200"></b-img>
+      <h1 class="mb-4 bg-white rounded-lg p-2 font-weight-bold">Here is the subject title</h1>
+    </b-col>
+    <b-col cols="12" lg="8">
+      <SortingNav />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </b-col>
+    <b-col cols="12" lg="4" tag="aside" class="bg-white shadow rounded-lg p-4 h-100 text-left">
+      <h2 class="h3 mb-3">A propos du sujet</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Aperiam totam illo nulla, iure quas sed explicabo velit,
+          mollitia id iste placeat dolorum libero dicta natus minus
+           autem enim qui harum.</p>
+      <p class="d-inline-block mr-5"><span class="h4">320</span><br>Membres</p>
+      <p class="d-inline-block ml-5"><span class="h4">15</span><br>en ligne</p>
+      <b-button block variant="info">Rejoindre</b-button>
+      <p><small>Crée le 16 janvier 2021</small></p>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue';
+import SortingNav from '@/components/SortingNav.vue';
+import Post from '@/components/Post.vue';
 
 export default {
   name: 'Subject',
-  components: {},
+  components: {
+    SortingNav,
+    Post,
+  },
 };
 </script>
 
