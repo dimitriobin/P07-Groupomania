e7568431bada59eff51b2c206d6c31167d82142a
Dimitri Obin <dimitriobin@gmail.com>
2020-11-24 09:43:56 +0100

    Integration de la structure en tabs

--- front_end/src/views/PersonalAccount.vue
+++ front_end/src/views/PersonalAccount.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="PersonalAccount">
-    <h1>Here is the Personal Account page</h1>
+    <b-tabs content-class="mt-3" fill>
+      <b-tab title="Account" active>
+        <p>I'm the first tab</p>
+      </b-tab>
+      <b-tab title="Profile"><p>I'm the second tab</p></b-tab>
+      <b-tab title="Privacy"><p>I'm the tab with the very, very long title</p></b-tab>
+    </b-tabs>
   </div>
 </template>
 
