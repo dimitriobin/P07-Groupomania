b6a8e61d8ef4faa2493c8f7849cd9022190053fd
Dimitri Obin <dimitriobin@gmail.com>
2020-11-25 19:03:52 +0100

    refactoring des balises semantiques dans Post.vue

--- front_end/src/components/Post.vue
+++ front_end/src/components/Post.vue
@@ -1,8 +1,8 @@
 <template>
-    <b-row no-gutters id="post" tag="article" class="bg-white rounded-lg shadow p-4 mb-4">
+    <b-row no-gutters tag="article" class="bg-white rounded-lg shadow p-4 mb-4">
         <b-col cols="12" tag="header" class="mb-2">
             <b-row no-gutters align-v="center">
-                <b-avatar cols="3" src="https://picsum.photos/100" size="lg" class="mr-4"></b-avatar>
+                <b-avatar src="https://picsum.photos/100" size="lg" class="mr-4"></b-avatar>
                 <b-col class="text-left">
                     <p class="h4">John Doe</p>
                     <p class="h6">Politics</p>
@@ -10,10 +10,14 @@
             </b-row>
         </b-col>
         <b-col cols="12" class="my-3  text-left">
-            <p class="h4 font-weight-bold">Lorem ipsum dolor sit amet,
+            <h1 class="h4 font-weight-bold">Lorem ipsum dolor sit amet,
                 abitur vestibulum pretium ipsum, eget iaculis dolor convallis eg
-                et. Nulla molestie lectus quis felis elementum maximus.</p>
-            <b-img fluid class="w-100" src="https://picsum.photos/400/200"></b-img>
+                et. Nulla molestie lectus quis felis elementum maximus.</h1>
+            <b-img
+                fluid
+                class="w-100"
+                alt="Description de l'image"
+                src="https://picsum.photos/400/200"></b-img>
             <a href="#">https://www.reddit.com/</a>
             <p class="mb-0 text-muted"><small>Il Y A 4 HEURES</small></p>
         </b-col>
