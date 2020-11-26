5284844a5920c18d1a730be3635f115624a308ae
Dimitri Obin <dimitriobin@gmail.com>
2020-11-26 17:34:01 +0100

    refonte du positionnement des elements de la barre de naviguation

--- front_end/src/components/Header/Navbar.vue
+++ front_end/src/components/Header/Navbar.vue
@@ -1,40 +1,38 @@
 <template>
-    <b-navbar
-      toggleable="md">
+    <b-navbar class="flex-column flex-sm-row">
         <!-- Logo -->
         <b-navbar-brand
           tag="img"
-          :src="require('../../assets/icon-left-font-220.png')"
+          width="220"
+          :src="require('../../assets/icon-left-font-removebg-cropped.png')"
           alt="Logo de Groupomania"
           fluid>
         </b-navbar-brand>
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-         <b-collapse id="nav-collapse" is-nav>
-          <!-- Right aligned nav items -->
-          <b-navbar-nav justified align="end" class="w-100">
-            <SearchBar />
-            <div class="d-flex">
-              <b-nav-item-dropdown
-                right
-                no-caret>
-                <template v-slot:button-content>
-                  <b-icon icon="plus-circle" variant="dark" font-scale="1.5"></b-icon>
-                </template>
-                <b-dropdown-item href="#">Créer un post</b-dropdown-item>
-                <b-dropdown-item href="#">Créer un sujet</b-dropdown-item>
-              </b-nav-item-dropdown>
-              <b-nav-item to="/">
-                <b-icon icon="house-door-fill" variant="dark" font-scale="1.5"></b-icon>
-              </b-nav-item>
-              <b-nav-item>
-                <b-icon icon="chat-dots" variant="dark" font-scale="1.5"></b-icon>
-              </b-nav-item>
-              <b-nav-item to="/personal">
-                <b-icon icon="person-fill" variant="dark" font-scale="1.5"></b-icon>
-              </b-nav-item>
-            </div>
-          </b-navbar-nav>
-        </b-collapse>
+        <b-navbar-nav  align="end" class="w-100 align-items-center">
+          <!-- SEARCHBAR -->
+          <SearchBar />
+          <!-- ICONS -->
+          <div class="d-flex mx-auto mx-sm-0">
+            <b-nav-item-dropdown
+              right
+              no-caret>
+              <template v-slot:button-content>
+                <b-icon icon="plus-circle" variant="dark" font-scale="1.5"></b-icon>
+              </template>
+              <b-dropdown-item href="#">Créer un post</b-dropdown-item>
+              <b-dropdown-item href="#">Créer un sujet</b-dropdown-item>
+            </b-nav-item-dropdown>
+            <b-nav-item to="/">
+              <b-icon icon="house-door-fill" variant="dark" font-scale="1.5"></b-icon>
+            </b-nav-item>
+            <b-nav-item>
+              <b-icon icon="chat-dots" variant="dark" font-scale="1.5"></b-icon>
+            </b-nav-item>
+            <b-nav-item to="/personal">
+              <b-icon icon="person-fill" variant="dark" font-scale="1.5"></b-icon>
+            </b-nav-item>
+          </div>
+        </b-navbar-nav>
     </b-navbar>
 </template>
 
--- front_end/src/components/Header/SearchBar.vue
+++ front_end/src/components/Header/SearchBar.vue
@@ -1,10 +1,11 @@
 <template>
-    <b-nav-form id="searchBar" class="mx-md-auto">
+    <b-nav-form
+        id="searchBar"
+        class="mx-md-auto d-none d-md-block w-50">
         <label class="sr-only" for="searchBar">Name</label>
         <b-form-input
-            id="searchBar"
             size="sm"
-            class="my-3 my-md-0">
+            class="my-3 my-md-0 w-100">
         </b-form-input>
         <b-button size="sm" variant="link">Search</b-button>
     </b-nav-form>
@@ -19,7 +20,6 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 #searchBar {
-    width: 100%;
     position: relative;
     button {
         position: absolute;
