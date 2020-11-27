c0adf33d92415f1ba035f5b399706fb9e5ad6089
Dimitri Obin <dimitriobin@gmail.com>
2020-11-27 18:39:27 +0100

    W3C Validator's refactoring

--- front_end/src/views/PersonalAccount.vue
+++ front_end/src/views/PersonalAccount.vue
@@ -1,5 +1,6 @@
 <template>
   <section id="PersonalAccount">
+    <h1 class="h2 text-center mb-4">Vos données personnelles</h1>
     <b-tabs content-class="mt-3" fill>
       <!-- Subjects -->
       <b-tab title="Sujets suivis">
@@ -65,35 +66,35 @@
           </b-form-group>
 
           <b-form-group
-            id="input-group-3"
+            id="input-group-4"
             label="Nouveau mot de passe:"
             label-cols="2"
-            label-for="input-3">
+            label-for="input-4">
             <b-form-input
-              id="input-3"
+              id="input-4"
               placeholder="*********************"
             ></b-form-input>
           </b-form-group>
 
           <b-form-group
-            id="input-group-3"
+            id="input-group-5"
             label="Confirmer le mot de passe:"
             label-cols="2"
-            label-for="input-3">
+            label-for="input-5">
             <b-form-input
-              id="input-3"
+              id="input-5"
               placeholder="*********************"
             ></b-form-input>
           </b-form-group>
 
           <b-form-group
-          id="input-group-4"
+          id="input-group-6"
           label="Photo de profil:"
           label-cols="2"
-          label-for="input-4"
+          label-for="input-6"
           class="d-flex">
             <b-form-file
-                id="input-4"
+                id="input-6"
                 :state="Boolean(/*file1*/)"
                 placeholder="Choose a file or drop it here..."
                 drop-placeholder="Drop file here..."
@@ -103,24 +104,24 @@
           </b-form-group>
 
           <b-form-group
-            id="input-group-5"
+            id="input-group-7"
             label="Date de naissance:"
             label-cols="2"
-            label-for="input-5">
+            label-for="input-7">
             <b-form-datepicker
-            id="input-5"
+            id="input-7"
             placeholder="17-02-1984">
             </b-form-datepicker>
           </b-form-group>
 
           <b-form-group
-            id="input-group-1"
+            id="input-group-8"
             label="Email des responsables:"
             label-cols="2"
-            label-for="input-6"
+            label-for="input-8"
           >
             <b-form-input
-              id="input-6"
+              id="input-8"
               type="email"
               placeholder="parents@mail.com"
             ></b-form-input>
@@ -131,7 +132,7 @@
       <!-- Privacy -->
       <b-tab title="Privacy">
         <b-form>
-          <b-form-group>
+          <b-form-group description="rgpd">
             <b-form-checkbox-group id="checkboxes" stacked>
               <b-form-checkbox value="restricted">
                 Restreindre l'utilisation de mes données
