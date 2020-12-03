4d1d20640c931779c2b24535cef244bdd88e9f72
Dimitri Obin <dimitriobin@gmail.com>
2020-12-03 13:48:06 +0100

    Define all request's functions for posts

--- front_end/src/services/PostDataService.js
+++ front_end/src/services/PostDataService.js
@@ -1,8 +1,32 @@
 import http from "../http-common";
 
 class PostDataService {
+  create(data) {
+    return http.post('/posts', data);
+  }
+
   getAll() {
     return http.get('/posts');
+  }
+
+  getAllByUser(id) {
+    return http.get(`/posts/user/${id}`);
+  }
+
+  getAllBySubject(id) {
+    return http.get(`/posts/subject/${id}`);
+  }
+
+  update(id, data) {
+    return http.put(`/posts/${id}`, data);
+  }
+
+  delete(id) {
+    return http.delete(`/posts/${id}`);
+  }
+
+  findByKeyword(keyword) {
+    return http.get(`/posts?keyword=${keyword}`);
   }
 };
 
