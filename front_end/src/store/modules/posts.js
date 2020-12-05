0af4c15bdd98394aae47a600f5d8bf6117e1a3ec
Dimitri Obin <dimitriobin@gmail.com>
2020-12-05 08:51:36 +0100

    add the authorization header in requests

--- front_end/src/store/modules/posts.js
+++ front_end/src/store/modules/posts.js
@@ -9,14 +9,14 @@ const getters = {
 };
 const actions = {
   addPost({ commit }, data) {
-    http.post('/posts', data)
+    http.post('/posts', data, { headers: authHeader() })
       .then((res) => {
         commit('newPost', res.data);
       })
       .catch((err) => { console.log(err); });
   },
   fetchAllPosts({ commit }) {
-    http.get('/posts')
+    http.get('/posts', { headers: authHeader() })
       .then((res) => {
         commit('setAllPosts', res.data);
       })
