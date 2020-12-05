f1e549f42ddeaf3f073df91e3bce70468353607e
Dimitri Obin <dimitriobin@gmail.com>
2020-12-05 08:46:23 +0100

    create a store module for authentication

---
+++ front_end/src/store/modules/auth.js
@@ -0,0 +1,64 @@
+import http from '../../http-common';
+
+const storedUser = JSON.parse(localStorage.getItem('user'));
+const initialState = storedUser
+  ? { status: { loggedIn: true }, storedUser }
+  : { status: { loggedIn: false }, storedUser: null };
+
+export default {
+  namespaced: true,
+  state: initialState,
+  actions: {
+    login({ commit }, user) {
+      http.post('/users/login', user)
+        .then((response) => {
+          console.log(response.data);
+          if (response.data.token) {
+            localStorage.setItem('user', JSON.stringify(response.data));
+          }
+          commit('loginSuccess', response.data);
+          this.$router.push('Home');
+        })
+        .catch((error) => {
+          console.log(error);
+          commit('loginFailure');
+        });
+    },
+    logout({ commit }) {
+      localStorage.removeItem('user');
+      commit('logout');
+    },
+    register({ commit }, user) {
+      console.log(user);
+      http.post('/users/signup', user)
+        .then((res) => {
+          console.log(res);
+          commit('registerSuccess');
+        })
+        .catch((error) => {
+          console.log(error);
+          commit('registerFailure');
+        });
+    },
+  },
+  mutations: {
+    loginSuccess(state, user) {
+      state.status.loggedIn = true;
+      state.user = user;
+    },
+    loginFailure(state) {
+      state.status.loggedIn = false;
+      state.user = null;
+    },
+    logout(state) {
+      state.status.loggedIn = false;
+      state.user = null;
+    },
+    registerSuccess(state) {
+      state.status.loggedIn = false;
+    },
+    registerFailure(state) {
+      state.status.loggedIn = false;
+    },
+  },
+};
