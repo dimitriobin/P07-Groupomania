7943bd60b3f8e84da9268b6a1c37569d0a3d2fa1
Dimitri Obin <dimitriobin@gmail.com>
2020-12-05 09:05:06 +0100

    add a beforeEach hook in the rooter to check auth in each naviguation

--- front_end/src/router/index.js
+++ front_end/src/router/index.js
@@ -42,4 +42,18 @@ const router = new VueRouter({
   routes,
 });
 
+router.beforeEach((to, _from, next) => {
+  const publicPages = ['/login'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+
+  // trying to access a restricted page + not logged in
+  // redirect to login page
+  if (authRequired && !loggedIn) {
+    next('/login');
+  } else {
+    next();
+  }
+});
+
 export default router;
