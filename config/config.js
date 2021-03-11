efdfde471a8997b3c41d24e0033217604650fe27
Dimitri Obin <dimitriobin@gmail.com>
2021-03-11 17:54:03 +0100

    fix bug

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": process.env.DB_DIALECT,
-    "use_env_variable": false,
+    use_env_variable: false,
     logging: false
   }
 }
