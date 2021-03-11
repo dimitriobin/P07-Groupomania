04a49b21e88eab0b6eadb0b22a5bcab94019ef6d
Dimitri Obin <dimitriobin@gmail.com>
2021-03-11 18:14:56 +0100

    reset the config for sequelize

--- config/config.js
+++ config/config.js
@@ -1,12 +1,9 @@
 'use strict'
 module.exports = {
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT,
-    use_env_variable: false,
-    logging: false
-  }
+  "username": process.env.DB_USER,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_NAME,
+  "host": process.env.DB_HOST,
+  "dialect": process.env.DB_DIALECT,
+  logging: false
 }
