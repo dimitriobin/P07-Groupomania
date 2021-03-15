23c56fe0718dbc13f646cba7d53bfd02c518cc2b
Dimitri Obin <dimitriobin@gmail.com>
2021-03-15 15:59:23 +0100

    add the csp https directive

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.use(helmet({
     contentSecurityPolicy: {
         directives: {
             ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-            "img-src": ["'self'", "blob:", "data:"],
+            "img-src": ["'self'", "blob:", "data:", "https:"],
           },
     }
 }));
