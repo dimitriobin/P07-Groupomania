f498c9da8ce3b2c59d9f5f9d8107eccc617bb774
Dimitri Obin <dimitriobin@gmail.com>
2021-03-16 15:52:55 +0100

    configure the new middleware

--- middlewares/multer-config.js
+++ middlewares/multer-config.js
@@ -1,23 +1,50 @@
 'use strict'
+// const multer = require('multer');
+
+// const MIME_TYPES = {
+//     'image/jpg': 'jpg',
+//     'image/jpeg': 'jpg',
+//     'image/png': 'png'
+// };
+
+// const storage = multer.diskStorage({
+//     destination: (req, file, callback) => {
+//         callback(null, 'images');
+//     },
+//     filename: (req, file, callback) => {
+//         const extension = MIME_TYPES[file.mimetype];
+//         const name = file.originalname.split(' ').join('_').slice(0, file.originalname.indexOf('.'));
+//         callback(null, name + Date.now() + '.' + extension);
+//     }
+// });
+
+// module.exports = multer({
+//     storage: storage
+// }).single('image_url');
+
+const aws = require('aws-sdk');
 const multer = require('multer');
+const multerS3 = require('multer-s3');
 
-const MIME_TYPES = {
-    'image/jpg': 'jpg',
-    'image/jpeg': 'jpg',
-    'image/png': 'png'
-};
-
-const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, 'images');
-    },
-    filename: (req, file, callback) => {
-        const extension = MIME_TYPES[file.mimetype];
-        const name = file.originalname.split(' ').join('_').slice(0, file.originalname.indexOf('.'));
-        callback(null, name + Date.now() + '.' + extension);
-    }
+aws.config.update({
+    secretAccessKey: process.env.AWSSecretKey,
+    accessKeyId: process.env.AWSAccessKeyId,
+    region: 'eu-west-3'
 });
 
-module.exports = multer({
-    storage: storage
-}).single('image_url');
+const s3 = new aws.S3();
+ 
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'groupomania',
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: file.fieldname});
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString())
+    }
+  })
+});
+
+module.exports = upload;
