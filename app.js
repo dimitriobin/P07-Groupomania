3300e7aa40602b810b6691831d49e2e781c9fdb4
Dimitri Obin <dimitriobin@gmail.com>
2021-03-16 18:41:22 +0100

    update helmet options to permit load from aws bucket url

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.use(helmet({
     contentSecurityPolicy: {
         directives: {
             ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-            "img-src": ["'self'", "blob:", "data:"],
+            "img-src": ["'self'", "blob:", "data:", "https://groupomania.s3.eu-west-3.amazonaws.com/*"],
           },
     }
 }));
